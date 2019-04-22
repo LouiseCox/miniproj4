64635e038f7ac3d11e26eab9bb8115634bd2d839
LouiseCox <49623459+LouiseCox@users.noreply.github.com>
2019-04-22 13:28:20 -0600

    Upload1

---
+++ JSforMiniProj4.js
@@ -0,0 +1,39 @@
+        var grades = [n1, n2, n3];
+        var i;
+
+        function calculateAverage(n1, n2, n3)
+        {
+            average = (n1 + n2 + n3)/3
+            return average    
+        }
+        
+        function convertAlphatoNumeric(GPA)
+        {
+            if (i == "A")
+            {
+                i = 100
+            }
+
+            if (i == "B")
+            {
+                i = 85
+            }
+
+            if (i == "C")
+            {
+                i = 70
+            }
+
+            if (i == "D")
+            {
+                i = 60
+            }
+        }
+
+        function myGPA(a1, a2, a3)
+        {
+
+        }
+
+        document.write (myGPA);
+        
