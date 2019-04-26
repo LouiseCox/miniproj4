cb8d4d92c4e7b806936012aba9bb280bac51658c
LouiseCox <49623459+LouiseCox@users.noreply.github.com>
2019-04-26 15:31:16 -0600

    Updated code for miniProj4

--- JSforMiniProj4.js
+++ JSforMiniProj4.js
@@ -1,39 +1,20 @@
-        var grades = [n1, n2, n3];
-        var i;
+function calculateAverage(n1, n2, n3){
+    return (n1 + n2 + n3) / 3;
+}
 
-        function calculateAverage(n1, n2, n3)
-        {
-            average = (n1 + n2 + n3)/3
-            return average    
+function convertAlphaToNumeric(GPA) {
+    var grades = ["A","B","C","D"];
+    var marks = [100,85,70,60];
+    var mark = 0;
+    for (grade in grades) {
+        if (GPA === grades[grade]) {
+            mark = marks[grade];
+            break;
         }
-        
-        function convertAlphatoNumeric(GPA)
-        {
-            if (i == "A")
-            {
-                i = 100
-            }
+    }
+    return mark;
+}
 
-            if (i == "B")
-            {
-                i = 85
-            }
-
-            if (i == "C")
-            {
-                i = 70
-            }
-
-            if (i == "D")
-            {
-                i = 60
-            }
-        }
-
-        function myGPA(a1, a2, a3)
-        {
-
-        }
-
-        document.write (myGPA);
-        
+function myGPA (a1, a2, a3) {
+    return calculateAverage (convertAlphaToNumeric(a1),convertAlphaToNumeric(a2),convertAlphaToNumeric(a3));
+}
